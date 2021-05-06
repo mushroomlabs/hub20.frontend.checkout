63890325c7eb70e451403ccd3d85e4b456b62381
Raphael Lullis <raphael@mushroomlabs.com>
2021-05-07 01:41:36 +0200

    (feat) ensure that user can choose different currencies

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <div>
       <div><label>Server URL</label> <input type="text" v-model="serverUrl" /></div>
       <div><label>Store ID</label><input type="text" v-model="storeId" /></div>
-      <div><label>Amount (USD)</label><input type="number" v-model="amount" /></div>
-      <button v-on:click="startCheckout">Start Checkout</button>
+      <div><label>Amount ({{ currency }})</label><input type="number" v-model="amount" /></div>
+      <button v-on:click="initializeCheckout">Start Checkout</button>
     </div>
     <hr />
     <Checkout v-if="isLoaded" />
@@ -23,13 +23,15 @@ export default {
   data() {
     return settings
   },
-  computed: {...mapGetters(['isLoaded'])},
+  computed: {
+    ...mapGetters(['isLoaded']),
+  },
   methods: {
-    startCheckout() {
+    initializeCheckout() {
       this.$store.dispatch('initialize', {
         serverUrl: this.serverUrl,
         storeId: this.storeId,
-        charge: {amount: this.amount, currencyCode: 'USD'},
+        charge: {amount: this.amount, currencyCode: this.currency},
       })
     },
   },
